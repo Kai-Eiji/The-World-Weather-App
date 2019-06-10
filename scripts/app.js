affa46b094be2f765156d6392f84ba3bd762a691
Kai-Eiji <kaiburkholder.jp@gmail.com>
2019-06-09 21:01:21 -0700

    finished

--- scripts/app.js
+++ scripts/app.js
@@ -3,20 +3,31 @@ const card = document.querySelector('.card');
 const details = document.querySelector('.details');
 const time = document.querySelector('img.time');
 const icon = document.querySelector('.icon img');
+const fiveWeather = document.querySelector(".five-weather");
 const forecast = new Forecast();
+
 
 const updateUI = (data) => {
   // destructure properties
-  const { cityDets, weather } = data;
 
+  const {cityDets, weather, weather5Day} = data;
+  // this is same as ...
+              // cityDets = data.cityDets
+              // weather = data.weather
+              // weather5Day = data.weather5Day
+  console.log(weather);
+  console.log(weather5Day);
+
+  
   // update details template
   details.innerHTML = `
-    <h5 class="my-3">${cityDets.EnglishName}</h5>
-    <div class="my-3">${weather.WeatherText}</div>
-    <div class="display-4 my-4">
-      <span>${weather.Temperature.Metric.Value}</span>
-      <span>&deg;C</span>
-    </div>
+      <h1 class="my-3">${cityDets.EnglishName}</h1>
+      <div class="my-3 text-25">${weather.WeatherText}</div>
+      <div class="display-3 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+      </div>
+    
   `;
 
   // update the night/day & icon images
@@ -26,11 +37,64 @@ const updateUI = (data) => {
   const timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
   time.setAttribute('src', timeSrc);
 
+  fiveWeather.innerHTML = " ";
+
+  const weather4day = weather5Day.slice(1,5);
+  console.log(weather4day);
+
+  weather4day.forEach( e => {
+    const weatherIcon = `img/icons/${e.Day.Icon}.svg`;
+    let d = new Date(e.Date);
+
+    var weekday = new Array(7);
+    weekday[0] =  "Sun";
+    weekday[1] = "Mon";
+    weekday[2] = "Tue";
+    weekday[3] = "Wed";
+    weekday[4] = "Thu";
+    weekday[5] = "Fri";
+    weekday[6] = "Sat";
+
+    var n = weekday[d.getDay()];
+
+    const fiveDayHTML = `
+    <div class="card">
+    <img src=${weatherIcon} class="card-img-top" alt="...">
+    <div class="card-body day">
+   
+    <h1 class="text-center">${d.getMonth()+1}/${d.getDate()} (${n})</h1>
+    <h2 class="hot" style="text-align:left;float:left;">${FtoC(e.Temperature.Maximum.Value)}&deg;C</h2> 
+    <h2 class="cold" style="text-align:right;float:right;">${FtoC(e.Temperature.Minimum.Value)}&deg;C</h2> 
+    
+
+    </div>`;
+
+    fiveWeather.innerHTML += fiveDayHTML;
+
+    
+
+    console.log(e.Date,d);
+    console.log(d.getDate());
+    console.log(d.getMonth());
+  //1560060000000
+
+
+
+  } );
+
+  
+
+
+
+
   // remove the d-none class if present
   if(card.classList.contains('d-none')){
     card.classList.remove('d-none');
   }
 };
+
+const FtoC = ( F => Math.round((F -32) * 5 /9));
+
 
 cityForm.addEventListener('submit', e => {
   // prevent default action
@@ -51,7 +115,7 @@ cityForm.addEventListener('submit', e => {
 });
 
 if(localStorage.getItem('city')){
-  forecast.updateCity(localStorage.getItem('city'))
+  forecast.updateCity(localStorage.getItem('city')) 
     .then(data => updateUI(data))
     .catch(err => console.log(err));
 }
--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,25 +1,34 @@
 class Forecast {
   constructor(){
-    this.key = 'ze6Safuk5kmDJBxBLrl7GV6QXCMEhJmP';
+    this.key = '4TU9lobSYp9fL7cv5VZV7ADfMYBt9AYY';
     this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
     this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    this.city5DayURI = "http://dataservice.accuweather.com/forecasts/v1/daily/5day/";
   }
   async updateCity(city){
     const cityDets = await this.getCity(city);
     const weather = await this.getWeather(cityDets.Key);
-    return { cityDets, weather };
+    const weather5Day = await this.get5dayWeather(cityDets.Key);
+    return { cityDets, weather, weather5Day };
   }
-  async getWeather(id){
-    const query = `${id}?apikey=${this.key}`;
+  async getWeather(id){ // id = city Key number
+    const query = `${id}?apikey=${this.key}`; // ?apikey=${api key parameter(account key)}
     const response = await fetch(this.weatherURI + query);
     const data = await response.json();
-    console.log(data);
     return data[0];
   }
   async getCity(city){
-    const query = `?apikey=${this.key}&q=${city}`;
+    const query = `?apikey=${this.key}&q=${city}&details=true`;
     const response = await fetch(this.cityURI + query);
     const data = await response.json();
-    return data[0];
+    return data[0]; 
+  }
+
+  async get5dayWeather(id){
+    const query = `${id}?apikey=${this.key}`;
+    const response = await fetch(this.city5DayURI + query);
+    const data = await response.json();
+    return data.DailyForecasts;
+
   }
 }
